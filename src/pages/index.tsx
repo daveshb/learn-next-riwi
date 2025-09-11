b155d7e8545132f93a8b14f841ef47113728ebea
David Henao <daves.hb@hotmail.com>
2025-09-11 07:12:00 -0500

    add fizzbuzz function2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,26 +12,24 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const product: string = "moto";
+  console.log(product);
 
-  const product:string = "moto";
-  console.log(product)
-
-function fizzBuzz(limit: number): void {
-  for (let i = 1; i <= limit; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
+  function fizzBuzz(limit: number): void {
+    for (let i = 1; i <= limit; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
     }
   }
-}
 
-fizzBuzz(200);
-
+  fizzBuzz(200);
 
   return (
     <div
